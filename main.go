e1116158a6234fe90a284ae3d2e52e2135bce0bf
Brad Cypert <brad.cypert@gmail.com>
2024-07-13 11:35:53 -0400

    Wire up delete flag, list flag, fix bug opening unfound bookmarks

--- main.go
+++ main.go
@@ -21,6 +21,8 @@ type Input struct {
 	Tags      []string
 	Search    bool
 	DeleteAll bool
+	Delete    bool
+	List      bool
 }
 
 func main() {
@@ -32,6 +34,10 @@ func main() {
 
 	if input.DeleteAll {
 		handleDeleteAll()
+	} else if input.List {
+		handleList()
+	} else if input.Delete && input.Bookmark != "" {
+		handleDelete(input)
 	} else if input.Path != "" {
 		handleStore(input)
 	} else if input.Search {
@@ -48,6 +54,17 @@ func handleDeleteAll() {
 	}
 }
 
+func handleDelete(input Input) {
+	file, err := getBookmarkFile(os.O_APPEND | os.O_RDWR)
+	if err != nil {
+		logger.Error(err.Error())
+	}
+	err = DeleteBookmark(file, input.Bookmark)
+	if err != nil {
+		logger.Error(err.Error())
+	}
+}
+
 func handleStore(input Input) {
 	file, err := getBookmarkFile(os.O_APPEND | os.O_WRONLY)
 	if err != nil {
@@ -57,6 +74,23 @@ func handleStore(input Input) {
 	if err != nil {
 		logger.Error(err.Error())
 	}
+}
+
+func handleList() {
+	file, err := getBookmarkFile(os.O_RDONLY)
+	if err != nil {
+		logger.Error(err.Error())
+	}
+	items, err := SearchBookmarks(file, "")
+	if err != nil {
+		logger.Error(err.Error())
+	}
+	tbl := table.New("Bookmark", "Path", "Tags")
+	for _, item := range items {
+		tbl.AddRow(item.Value, item.Path, strings.Join(item.Tags, ","))
+	}
+
+	tbl.Print()
 }
 
 func handleSearch(input Input) {
@@ -84,19 +118,21 @@ func handleOpen(input Input) {
 	item, err := GetBookmark(file, input.Bookmark)
 	if err != nil {
 		logger.Error(err.Error())
+		return
 	}
 
 	err = openExternal(item.Path)
 	if err != nil {
 		logger.Error(err.Error())
 	}
-
 }
 
 func parseFlags() (Input, error) {
 	tags := flag.String("tags", "", "Comma Separated tags for faster searching")
 	search := flag.Bool("search", false, "Search for existing bookmarks against the provided query")
 	deleteAll := flag.Bool("deleteAll", false, "Ignore other flags and delete the bookmark database")
+	d := flag.Bool("delete", false, "Delete the bookmark by the provided key")
+	l := flag.Bool("list", false, "List all bookmarks")
 	t := strings.Split(*tags, ",")
 
 	flag.Parse()
@@ -104,7 +140,7 @@ func parseFlags() (Input, error) {
 	bookmark := flag.Arg(0)
 	path := flag.Arg(1)
 
-	if bookmark == "" && !*deleteAll {
+	if bookmark == "" && !(*deleteAll || *l) {
 		return Input{}, errors.New("a bookmark value is required. This value is the argument directly after the \"book\" command")
 	}
 
@@ -113,6 +149,8 @@ func parseFlags() (Input, error) {
 		Tags:      t,
 		Bookmark:  bookmark,
 		DeleteAll: *deleteAll,
+		Delete:    *d,
 		Search:    *search,
+		List:      *l,
 	}, nil
 }
